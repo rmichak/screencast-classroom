92af2bc540e355ea5189d00510739853d4a3c42f
Randy Michak <rmichak@gmail.com>
2026-03-11 20:09:44 +0000

    Fix: exclude node_modules and certs from repo

--- node_modules/mediasoup/worker/fuzzer/src/RTC/RTCP/FuzzerFeedbackPsPli.cpp
+++
@@ -1,9 +0,0 @@
-#include "RTC/RTCP/FuzzerFeedbackPsPli.hpp"
-
-void Fuzzer::RTC::RTCP::FeedbackPsPli::Fuzz(::RTC::RTCP::FeedbackPsPliPacket* packet)
-{
-	// packet->Dump();
-	packet->Serialize(::RTC::RTCP::SerializationBuffer);
-	packet->GetCount();
-	packet->GetSize();
-}
